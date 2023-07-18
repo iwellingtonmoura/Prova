55e087fdc742d36d254b01be1ef3918c2dc49a0f
iwellingtonmoura@gmail.com <wellingtonmoura@gmail.com>
2023-07-17 21:06:28 -0300

    Iniciando a implementação do Redis

--- Prova/Program.cs
+++ Prova/Program.cs
@@ -1,70 +1,72 @@
-﻿using System.Text.Json;
-using AutoWrapper;
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.WebUtilities;
-using Prova.DataManager;
-using Prova.Extensions;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿    using System.Text.Json;
+    using AutoWrapper;
+    using FluentValidation.AspNetCore;
+    using Microsoft.AspNetCore.WebUtilities;
+    using Prova.DataManager;
+    using Prova.Extensions;
+    using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    var builder = WebApplication.CreateBuilder(args);
 
 
-//Adicionando a classe que injeta os serviços.
-builder.Services.AddProvaWorker(builder.Configuration);
 
-//builder.Services.AddMemoryCache();
+    // Add services to the container.
 
-var app = builder.Build();
+    builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//   app.UseSwagger();
-
-//}
-
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Aplicacao de Teste v1");
-    options.DisplayRequestDuration();
-});
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
 
-//app.UseHealthChecks("/status",
-//    new HealthCheckOptions()
-//    {
-//        ResponseWriter = async (context, report) =>
-//        {
-//            var result = JsonSerializer.Serialize(
-//                new
-//                {
-//                    currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
-//                    statusApplication = report.Status.ToString(),
-//                    HealthChecks = report.Entries.Select(s => new
-//                    {
-//                        check = s.Key,
-    
-//                        status = Enum.GetName(typeof(HealthStatus), s.Value.Status)
-//                    })
-//                });
-//        }
-//    });
+    //Adicionando a classe que injeta os serviços.
+    builder.Services.AddProvaWorker(builder.Configuration);
+    builder.Services.AddHealthChecks();
 
-app.UseSwaggerUI();
-app.UseAuthorization();
-app.UseAuthentication();
-app.UseApiResponseAndExceptionWrapper(new AutoWrapperOptions { IsDebug = true });
-app.MapControllers();
-app.Run();
+    //builder.Services.AddMemoryCache();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
+    //   app.UseSwagger();
+
+    //}
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Aplicacao de Teste v1");
+        options.DisplayRequestDuration();
+    });
+
+
+    app.UseHealthChecks("/status",
+        new HealthCheckOptions()
+        {
+            ResponseWriter = (context, report) =>
+            {
+                var result = JsonSerializer.Serialize(
+                    new
+                    {
+                        currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                        statusApplication = report.Status.ToString(),
+                        HealthChecks = report.Entries.Select(s => new
+                        {
+                            check = s.Key,
+
+                            status = Enum.GetName(typeof(HealthStatus), s.Value.Status)
+                        })
+                    });
+                return Task.CompletedTask;
+            }
+        });
+
+    app.UseSwaggerUI();
+    app.UseAuthorization();
+    app.UseAuthentication();
+    app.UseApiResponseAndExceptionWrapper(new AutoWrapperOptions { IsDebug = true });
+    app.MapControllers();
+    app.Run();
 
---
+++ Prova/Services/CacheDistribuido.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+namespace Prova.Services
+{
+	public class CacheDistribuido
+	{
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis-18603.c74.us-east-1-4.ec2.cloud.redislabs.com:18603,password=<password>");
+        static async Task Main(string[] args)
+        {
+            var db = redis.GetDatabase();
+        }
+    }
+}
+
+
